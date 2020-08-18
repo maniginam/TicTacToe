bd5b56342a1782cbebd61bb2e8e10ff2616d83ec
maniginam <maniginam@gmail.com>
2020-08-18 17:05:30 -0500

    added player options for hvh, hvc, or cvc

--- src/ttt/core.clj
+++ src/ttt/core.clj
@@ -5,40 +5,24 @@
     [ttt.board :refer :all]
     [ttt.human-box-input :refer :all]))
 
-(defn game-results [winner human]
+(defn game-results [winner]
   (let [results-msg
         (cond (zero? winner) "Cat's Game"
-              (= winner human) "You Win!"
-              :else "I Win!")]
+              (= winner 1) "X Wins!"
+              :else "O Wins!")]
     (println results-msg)
     results-msg))
 
 (defn computer-turn [board player]
   (let [box (play-optimal-box board player)]
-    (println "I choose box" box)
+    (println "Computer plays box" box)
     box))
 
-(defn initiate-human-turn [board]
-  (println "Your Turn")
-  (draw-board board))
-
-(defn human-turn [board player is-test?]
-  (initiate-human-turn board)
-  (if is-test?
-    (play-optimal-box board player)
-    (get-human-selection board)))
-
-(defn play-ttt [human is-test?]
-  (let [computer (get-next-player human)]
-    (loop [board board
-           player 1]
-      (cond (is-win? board) (do (draw-board board) (get-next-player player))
-            (full-board? board) 0
-            :else (let [box-played
-                        (if (= player human) (human-turn board human is-test?) (computer-turn board computer))]
-                    (recur
-                      (put-piece-on-board board box-played (get-player-piece player))
-                      (get-next-player player)))))))
+(defn human-turn [board player human]
+  (if (= human player)
+    (println "Your Turn")
+    (println (str (get-player-piece player) "'s Turn")))
+  (get-human-selection board))
 
 (defn offer-x-or-o []
   (println "X goes first.  Do you want to be X or O")
@@ -51,11 +35,47 @@
                                     (set-human-game-piece (inc tries) (offer-x-or-o)))
                     :else (do (println "Nevermind, I'll go first.") 2))))
 
-(defn welcome [human]
+(defn get-box-played [board player human]
+  (cond (nil? human) (human-turn board player human)
+        (= human 0) (computer-turn board player)
+        :else (if (= human player) (human-turn board player human) (computer-turn board player))))
+
+(defn play-ttt [players human]
+  (loop [board board
+         player 1]
+    (cond (is-win? board) (get-next-player player)
+          (full-board? board) 0
+          :else (let [box-played (get-box-played board player human)]
+                  (draw-board (put-piece-on-board board box-played (get-player-piece player)))
+                  (recur (put-piece-on-board board box-played (get-player-piece player))
+                         (get-next-player player))))))
+
+(defn who-is-playing [tries]
+  (if (= tries 0)
+    (do (println "0 Computer v Computer")
+        (println "1 Human v Computer")
+        (println "2 Human v Human")))
+  (println "How many humans are playing?  Please enter 0, 1, or 2")
+  (let [players (try (Integer/parseInt (read-line))
+                     (catch Exception e (println "Wrong Input!")
+                                        (who-is-playing 1)))]
+    (if (or (< players 0) (> players 2))
+      (do (println "Entry not an option...  ")
+          (who-is-playing 1))
+      players)))
+
+(defn start-game [players human]
   (println "Let's Play Tic-Tac-Toe!")
-  (let [human (if (nil? human) (set-human-game-piece 0 (offer-x-or-o)) human)
-        winner (play-ttt human false)]
-    (game-results winner human)))
+  (if (or (= players 2) (= human 1)) (draw-board board)))
+
+(defn welcome []
+  (println "Welcome to Tic-Tac-Toe!")
+  (let [players (who-is-playing 0)
+        human (cond (= players 0) 0
+                    (= players 1) (set-human-game-piece 0 (offer-x-or-o))
+                    :else nil)]
+    (start-game players human)
+    (game-results (play-ttt players human))))
 
 (defn -main []
-  (welcome nil))
+  (welcome))
